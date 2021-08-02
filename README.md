b3e70a9970523fdde4f389585e3130d1e6577b40
Santiago Alves <santiagoalvessouza@gmail.com>
2021-08-02 00:07:28 -0300

    update readme.md; update auto-docs; update cache-policies

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-from https://github.com/cvc-corp/Teste-API/tree/master/soa_osb_test-master
+#### from https://github.com/cvc-corp/Teste-API/tree/master/soa_osb_test-master
+
+#### to start local host run:
+
+ `./gradlew clean bootRun`
+
+#### access `localhost:8080/swagger-ui/` for API docs
+
+
+
+
--- src/main/java/br/com/cvccorp/hotelgateway/HotelGatewayApplication.java
+++ src/main/java/br/com/cvccorp/hotelgateway/HotelGatewayApplication.java
@@ -6,9 +6,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @EnableCaching
 @EnableSwagger2
@@ -25,6 +29,20 @@ public class HotelGatewayApplication {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.cvccorp.hotelgateway.resource"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(new ApiInfo(
+                        "Hotel Gateway",
+                        "from https://github.com/cvc-corp/Teste-API/tree/master/soa_osb_test-master",
+                        "1",
+                        "Terms of service",
+                        new Contact(
+                                "santiagoalves",
+                                "https://github.com/santiagoalves",
+                                null
+                        ),
+                        "license - 4free",
+                        "",
+                        Collections.emptyList()
+                ));
     }
 }
